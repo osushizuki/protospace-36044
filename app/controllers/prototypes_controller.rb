a76d17fe046b25a03de739ff4bf668c5a001061c
osushizuki <imaokanorihiko@icloud.com>
2021-09-25 18:00:12 +0900

    修正_prototypeコントローラー

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -10,11 +10,11 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    prototype = Prototype.new(prototype_params)
-    if prototype.save
-      redirect_to action: :index
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
     else
-      render "prototypes/new"
+      render :new
     end
   end
 
@@ -29,9 +29,9 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
-      redirect_to root_path
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
     else
       render :edit
     end
