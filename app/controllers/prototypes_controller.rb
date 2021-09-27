b8ae6616bccf8c03849335dc98ca24729ee32e4f
osushizuki <imaokanorihiko@icloud.com>
2021-09-27 20:21:10 +0900

    修正2回目

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,8 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_param, except: [:index, :new, :create]
   before_action :move_to_index, except: [:index, :show]
-  before_action :no_current_user, except: [:index, :show]
+  before_action :no_current_user, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -21,17 +22,15 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
+    @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    @prototype
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -40,12 +39,15 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
+    @prototype.destroy
     redirect_to root_path
   end
 
   private
+
+  def set_param
+    @prototype = Prototype.find(params[:id])
+  end
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
@@ -58,7 +60,6 @@ class PrototypesController < ApplicationController
   end
 
   def no_current_user
-    @prototype = Prototype.find(params[:id])
     unless current_user == @prototype.user
       redirect_to action: :index 
     end
